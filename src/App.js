81c69c96ed3d39466b9d7568062b79c686222a6d
FedericaI7 <federicaiuvara3@gmail.com>
2023-08-25 19:29:06 +0200

    Add useEffect and new useState monsterFiltered

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Card from "./Card";
 export default function App() {
   const [inputValue, setInputValue] = useState("");
   const [monster, setMonster] = useState([]);
+  const [monsterFiltered, setMonsterFiltered] = useState(monster);
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
@@ -12,11 +13,28 @@ export default function App() {
       .then((data) => {
         setMonster(data);
       });
+  }, []);
+
+  const changeValue = (event) => {
+    const valueOnInput = inputValue.event.value.toLowerCase();
+    setInputValue(valueOnInput);
+  };
+
+  useEffect(() => {
+    const filtro = monster.filter((mostro) => {
+      const checkMonster = mostro.name.toLowerCase();
+      return checkMonster.includes(inputValue.toLowerCase());
+    });
+    setMonsterFiltered(filtro);
   }, [monster, inputValue]);
 
   return (
     <div className="container--app">
-      <Header title={"Search the monster"} placeholder={"Search the monster"} />
+      <Header
+        title={"Search the monster"}
+        placeholder={"Search the monster"}
+        changeValue={changeValue}
+      />
       <Card stateMonster={monster} />
     </div>
   );
--- src/Header.jsx
+++ src/Header.jsx
@@ -5,7 +5,11 @@ export default function Header(props) {
   return (
     <div className="container--header">
       <h1 className="text--header">{props.title}</h1>
-      <input type="text" placeholder={props.placeholder} />
+      <input
+        type="text"
+        placeholder={props.placeholder}
+        onChange={props.changeValue}
+      />
     </div>
   );
 }
