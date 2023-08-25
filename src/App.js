97238ed224b451ea320e66600a88a10c11eb075e
FedericaI7 <federicaiuvara3@gmail.com>
2023-08-25 17:51:45 +0200

    Add: useEffect with fetch - and useState for Input

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import Card from "./Card";
 
 export default function App() {
+  const [inputValue, setInputValue] = useState("");
   const [monster, setMonster] = useState([]);
+
+  useState(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => {
+        setMonster(data);
+      });
+  }, [monster]);
+
   return (
     <div className="container--app">
       <Header title={"Search the monster"} placeholder={"Search the monster"} />
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,5 +1,18 @@
 import React from "react";
 
 export default function Card(props) {
-  return <div className="container--card"></div>;
+  return (
+    <div className="container--card">
+      {props.stateMonster.map((card) => {
+        <div className="card">
+          <h1>{card.name}</h1>
+          <img
+            src={`https://robohash.org/${card.id}?size=180x180`}
+            alt="monster illustration"
+          />
+          <p>{card.email}</p>
+        </div>;
+      })}
+    </div>
+  );
 }
