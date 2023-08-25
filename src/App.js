4d9feb7c6ab28eaaccd3cabab70e493df8580192
FedericaI7 <federicaiuvara3@gmail.com>
2023-08-25 17:16:16 +0200

    I added the Card component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
+import Card from "./Card";
 
 export default function App() {
+  const [monster, setMonster] = useState([]);
   return (
     <div className="container--app">
       <Header title={"Search the monster"} placeholder={"Search the monster"} />
+      <Card stateMonster={monster} />
     </div>
   );
 }
